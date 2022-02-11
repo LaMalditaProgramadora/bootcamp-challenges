41e11cf34cf18fedc7b561bcdba7d9e2fb070c9f
Jimena Alexandra Ruiz Cerna <jyellow363@gmail.com>
2022-02-10 20:04:52 -0500

    fix calculator /0

--- calculator-redux/src/redux/features/calculatorSlice.js
+++ calculator-redux/src/redux/features/calculatorSlice.js
@@ -25,13 +25,18 @@ export const calculatorSlice = createSlice({
       );
     },
     divide: (state, { payload: numbers }) => {
-      state.screen = parseInt(
-        parseInt(numbers.lastInputNumber) / parseInt(numbers.inputNumber)
-      );
+      if (parseInt(numbers.inputNumber) === 0) {
+        state.screen = 0;
+      } else {
+        state.screen = parseInt(
+          parseInt(numbers.lastInputNumber) / parseInt(numbers.inputNumber)
+        );
+      }
     },
   },
 });
 
-export const { add, subtract, multiply, divide, setScreen } = calculatorSlice.actions;
+export const { add, subtract, multiply, divide, setScreen } =
+  calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
